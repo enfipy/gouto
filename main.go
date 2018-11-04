b9cad13e91d356331e766e99198868073e886b8f
David Ackerman <enfipy@gmail.com>
2018-11-04 22:21:27 +0200

    Add flags

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
 
 	"github.com/fatih/color"
 )
 
 // Delay - milliseconds to wait before begin next job after a file change
 const Delay = 1000
+
+// Pattern - watched files extensions pattern
+const Pattern = `(.+\.go)$`
+
+var (
+	flagDirectory = flag.String("dir", ".", "Directory to watch for changes")
+	flagOutput    = flag.String("out", ".", "Output directory for binary after build")
+	flagRun       = flag.String("run", "", "Command to run after build")
+	flagBuild     = flag.String("build", "go build", "Command to rebuild after changes")
+)
 
 func colorSuccess(format string) string {
 	return color.GreenString(format)
@@ -18,6 +30,13 @@ func colorFail(format string) string {
 }
 
 func main() {
+	flag.Parse()
+
+	if *flagDirectory == "" {
+		log.Println(colorFail("Directory flag is required"))
+		os.Exit(1)
+	}
+
 	result := true
 
 	if result != false {
